7a441176d54eb0a1b21b457c16eb43ac92ec788d
PrantoMondol11 <mondolpranto83@gmail.com>
2026-06-09 05:29:52 -0500

    ci 17

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -23,8 +23,6 @@ print("DAGsHub URL:", dagshub_url)
 repo_name="SentimentOps"
 mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
 
-dagshub.init(repo_owner=repo_owner, repo_name=repo_name, mlflow=True)
-
 def load_model_info(file_path:str) -> dict:
     """Load model information and evaluation metrices from a specified path"""
     try:
